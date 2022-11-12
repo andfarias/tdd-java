693be96d39967fe321d37c354e8df4ea604fd0dc
Anderson Farias <andersonsantofarias@outlook.com>
2022-11-11 22:22:31 -0300

    Adicionado primeiro teste de unidade

---
+++ src/test/java/service/BonusServiceTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Anderson", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Anderson", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoParaSalario10000() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Anderson", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.0"), bonus);
+    }
+}
